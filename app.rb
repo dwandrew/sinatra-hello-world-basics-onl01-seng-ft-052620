5a4d33da47d9f8c0f32c7e1b13af944dc0c4ee91
dwandrew <danwandrew@gmail.com>
2020-06-27 09:38:22 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-
-
+  # get '/' do "Hello, World!"
+  # end
+  get '/' do "Just adding me some extra text to see if this works"
+  end
 end
